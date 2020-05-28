ea4f6d32f10596bf536dd045e3df53af3d13e440
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-05-28 19:08:53 +0200

    more insertions

--- InsertIntoSzpital.sql
+++ InsertIntoSzpital.sql
@@ -122,10 +122,10 @@ INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(
 INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(007, 10500, 'Chirurgia klatki piersiowej');
 INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(008, 14300, 'Chirurgia naczyniowa');
 INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(009, 17200, 'Chirurgia ogólna');
+
 INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(010, 03700, 'Chirurgia onkologiczna');
 INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(011, 06900, 'Chirurgia plastyczna');
 INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(012, 42069, 'Chirurgia szczêkowo-twarzowa');
-
 INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(013, 02100, 'Choroby p³uc');
 INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(014, 05300, 'Choroby p³uc dzieci');
 INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(015, 03700, 'Choroby wewnêtrzne');
@@ -133,6 +133,7 @@ INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(
 INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(017, 08200, 'Dermatologia i wenerologia');
 INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(018, 05600, 'Diabetologia');
 INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(019, 10100, 'Diagnostyka laboratoryjna');
+
 INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(020, 12300, 'Endokrynologia');
 INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(021, 06200, 'Endokrynologia ginekologiczna i rozrodczoœæ');
 INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(022, 03700, 'Endokrynologia i diabetologia dzieciêca');
@@ -144,3 +145,60 @@ INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(
 INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(027, 06900, 'Geriatria');
 INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(028, 09000, 'Ginekologia onkologiczna');
 INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(029, 04300, 'Hematologia');
+
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(030, 12300, 'Hipertensjologia');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(031, 06200, 'Immunologia kliniczna');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(032, 03700, 'Intensywna terapia');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(033, 06900, 'Kardiochirurgia');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(033, 07069, 'Kardiologia');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(034, 02300, 'Kardiologia dzieciêca');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(035, 07200, 'Medycyna lotnicza');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(036, 03700, 'Medycyna morska i tropikalna');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(037, 06900, 'Medycyna nuklearna');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(038, 09000, 'Medycyna paliatywna');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(039, 04300, 'Medycyna pracy');
+
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(040, 12300, 'Medycyna ratunkowa');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(041, 06200, 'Medycyna rodzinna');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(042, 03700, 'Medycyna s¹dowa');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(043, 06900, 'Medycyna sportowa');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(044, 07069, 'Mikrobiologia lekarska');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(045, 02300, 'Nefrologia');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(046, 07200, 'Nefrologia dzieciêca');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(047, 03700, 'Neonatologia');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(048, 06900, 'Neurochirurgia');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(049, 09000, 'Neurologia');
+
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(050, 04300, 'Neurologia dzieciêca');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(051, 12300, 'Neuropatologia');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(052, 06200, 'Okulistyka');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(053, 03700, 'Onkologia i hematologia dzieciêca');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(054, 06900, 'Onkologia kliniczna');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(055, 07069, 'Ortopedia i traumatologia narz¹du ruchu');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(056, 02300, 'Otorynolaryngologia');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(057, 07200, 'Otorynolaryngologia dzieciêca');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(058, 03700, 'Patomorfologia');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(059, 06900, 'Pediatria');
+
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(060, 09000, 'Pediatria metaboliczna');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(061, 04300, 'Perinatologia');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(062, 12300, 'Po³o¿nictwo i ginekologia');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(063, 06200, 'Psychiatria');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(064, 03700, 'Psychiatria dzieci i m³odzie¿y');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(065, 06900, 'Radiologia i diagnostyka obrazowa');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(066, 07069, 'Radioterapia onkologiczna');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(067, 02300, 'Rehabilitacja medyczna');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(068, 07200, 'Reumatologia');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(069, 03700, 'Seksuologia');
+
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(070, 06900, 'Toksykologia kliniczna');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(071, 09000, 'Transfuzjologia kliniczna');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(072, 04300, 'Transplantologia kliniczna');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(073, 12300, 'Urologia');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(074, 06200, 'Urologia dzieciêca');
+INSERT INTO szpital.dbo.specjalnosci (ID, min_stawka, opis_specjalnosci) VALUES(075, 03700, 'Zdrowie publiczne');
+
+GO
+/*
+SELECT * from szpital.dbo.specjalnosci;
+*/
